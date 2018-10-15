51c9f241c1962aab25a85da5a54702f8d3675e7f
Jim Haff <Jimmyhaff3@gmail.com>
2018-10-15 17:21:04 +0100

    connected controller

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app     = express();
 
 require('./db/db');
 
+const authorsController = require('./controllers/authors');
 
+// setting up the middleware for our controller
+// where every route will start with /authors
+app.use('/authors', authorsController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
