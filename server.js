11d7056b8b8e43fba2cdceef598a68b4facc6416
Jim Haff <Jimmyhaff3@gmail.com>
2018-10-15 17:03:59 +0100

    added connection to mongo

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app     = express();
 
+require('./db/db');
+
+
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 
 app.listen(3000, () => {
