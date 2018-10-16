f6dc6459e70118f84ffc37e757022887e01e1134
Jim Haff <Jimmyhaff3@gmail.com>
2018-10-16 23:23:34 +0100

    completed edit

--- controllers/articles.js
+++ controllers/articles.js
@@ -35,10 +35,19 @@ router.get('/:id', (req, res)=>{
 });
 
 router.get('/:id/edit', (req, res)=>{
-  Article.findById(req.params.id, (err, foundArticle)=>{
-
-    res.render('articles/edit.ejs', {
-      article: foundArticle
+  Article.findById(req.params.id, (err, foundArticle) => {
+    // find allAuthors to add them to the page
+    Author.find({}, (err, allAuthors) => {
+      // Find the AUthor that is tied to the article that we are finding
+      // in our route, so we can make sure they are selected on the drop down
+      // so the use knows who is the original owner of the article
+      Author.findOne({'articles._id': req.params.id}, (err, foundArticleAuthor) => {
+          res.render('articles/edit.ejs', {
+            article: foundArticle,
+            authors: allAuthors,
+            articleAuthor: foundArticleAuthor
+          });
+      });
     });
   });
 });
@@ -81,19 +90,45 @@ router.delete('/:id', (req, res)=>{
 });
 
 router.put('/:id', (req, res)=>{
+  // Check to see if the user, (req.body.authorId), changed the author
+  // if the did we'll need to update the newAuthro and remove the old author
+
+
   Article.findOneAndUpdate(req.params.id, req.body, {new: true}, (err, updatedArticle)=>{
     // Once we updated the article in the articles collection
     // then we want to update the article in Author.articles
     Author.findOne({'articles._id': req.params.id}, (err, foundAuthor) => {
       // so now that I've found the author that has the article
+      if(foundAuthor._id.toString() !== req.body.authorId){
+        // if they are not equal that means, the user changed the author
+        // so then we want to remove the article from the author that used
+        // to own it
+        foundAuthor.articles.id(req.params.id).remove()
+        foundAuthor.save((err, savedFoundAuthor) => {
+          // SO now that we removed the Article from the old authoer
+          // we want to find the New Author and add the update article to
+          // the new authors articles array
+          Author.findById(req.body.authorId, (err, newAuthor) => {
+            // so now that we've found the newAuthor,
+            // push the updatedArticle which if you'll look above was
+            // the response from the first database query
+            // Article.findOneAndUpdate
+            newAuthor.articles.push(updatedArticle);
+            newAuthor.save((err, savedNewAuthor) => {
+              res.redirect('/articles');
+            });
+          });
+        });
+      } else {
 
-      // Find that article and remove
-      foundAuthor.articles.id(req.params.id).remove();
-      // Then add the updateArticle to authors.articles array
-      foundAuthor.articles.push(updatedArticle);
-      foundAuthor.save((err, data) => {
-        res.redirect('/articles');
-      });
+          // Find that article and remove
+          foundAuthor.articles.id(req.params.id).remove();
+          // Then add the updateArticle to authors.articles array
+          foundAuthor.articles.push(updatedArticle);
+          foundAuthor.save((err, data) => {
+            res.redirect('/articles');
+          });
+      } // end of if else
     });
   });
 });
