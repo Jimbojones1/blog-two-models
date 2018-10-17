a6f19bea8afd857cf6c549c7b28fd25ff5d5886e
Jim Haff <Jimmyhaff3@gmail.com>
2018-10-17 18:08:07 +0100

    added async and await functionality

--- controllers/articles.js
+++ controllers/articles.js
@@ -4,135 +4,171 @@ const Article = require('../models/articles');
 const Author  = require('../models/authors');
 
 
-router.get('/', (req, res)=>{
-  Article.find({}, (err, foundArticles)=>{
-    res.render('articles/index.ejs', {
-      articles: foundArticles
-    });
-  })
+router.get('/', async (req, res)=>{
+
+       try  {
+
+            const foundArticles = await Article.find({});
+
+            res.render('articles/index.ejs', {
+              articles: foundArticles,
+            });
+
+          } catch (err){
+
+            res.send(err);
+
+          }
+
 });
 
+router.get('/new', async (req, res)=>{
+  try {
 
-router.get('/new', (req, res) => {
-  Author.find({}, (err, allAuthors) => {
-    res.render('articles/new.ejs', {
-      authors: allAuthors
-    });
-  })
+     const allAuthors = await Author.find();
 
-})
-
-router.get('/:id', (req, res)=>{
-  Article.findById(req.params.id, (err, foundArticle)=>{
-    Author.findOne({'articles._id': req.params.id}, (err, foundAuthor) => {
-      console.log(foundAuthor, ' this is foundAuthor')
-        res.render('articles/show.ejs', {
-          article: foundArticle,
-          author: foundAuthor
+      res.render('articles/new.ejs', {
+          authors: allAuthors
         });
-    });
-  });
+
+  } catch (err) {
+
+      res.send(err);
+  }
 });
 
-router.get('/:id/edit', (req, res)=>{
-  Article.findById(req.params.id, (err, foundArticle) => {
-    // find allAuthors to add them to the page
-    Author.find({}, (err, allAuthors) => {
-      // Find the AUthor that is tied to the article that we are finding
-      // in our route, so we can make sure they are selected on the drop down
-      // so the use knows who is the original owner of the article
-      Author.findOne({'articles._id': req.params.id}, (err, foundArticleAuthor) => {
-          res.render('articles/edit.ejs', {
+router.get('/:id', async (req, res)=>{
+
+
+  try {
+      const findArticle = Article.findById(req.params.id);
+      const findAuthor  = Author.findOne({'articles._id': req.params.id});
+
+      // Promise All returns an array of the repsonse from DB queries,
+      // Using array destructing to save the corresponding responses
+      // as the variables thisArticle, and foundAuthor
+      // the array destructering is the const [thisArticle, foundAuther]
+      // Basially what this is doing is creating varaibles for each index in the array that
+      // is returned from await Promise.all([findArticle, findAuthor])
+      //if you are still confused look up array destructering, its fancy new javascript
+      const [foundArticle, foundAuthor] = await Promise.all([findArticle, findAuthor]);
+
+      res.render('articles/show.ejs', {
+        article: foundArticle,
+        author: foundAuthor
+      });
+
+  } catch (err) {
+
+    next(err);
+  }
+
+});
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+
+      const findArticle = Article.findById(req.params.id);
+      const findAllAuthors = Author.find();
+      const findArticleAuthor = Author.findOne({'articles._id': req.params.id});
+
+      const [foundArticle, allAuthors, foundArticleAuthor] = await Promise.all([findArticle, findAllAuthors, findArticleAuthor]);
+
+      res.render('articles/edit.ejs', {
             article: foundArticle,
             authors: allAuthors,
             articleAuthor: foundArticleAuthor
           });
-      });
-    });
-  });
+
+  } catch (err) {
+
+      res.send(err);
+
+  }
+
 });
 
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
+   try {
 
-  // First we are finding the author that was chosen
-  // from the browser
-  Author.findById(req.body.authorId, (err, foundAuthor) => {
-    // Then we are creating the article from the browser,
-    // after we got the response from the Author query above ^
-    Article.create(req.body, (err, createdArticle) => {
+      const findAuthor = Author.findById(req.body.authorId);
+      const createArticle = Article.create(req.body);
 
-      // Then when we get the response from the article query
-      // we tie the article to the author like below
+      const [foundAuthor, createdArticle] = await Promise.all([findAuthor, createArticle]);
       foundAuthor.articles.push(createdArticle);
-      foundAuthor.save((err, data) => {
-        res.redirect('/articles')
-      });
-    });
-  });
+
+      await foundAuthor.save();
+      res.redirect('/articles');
+
+  } catch(err){
+    console.log('errroor')
+     res.send(err);
+  }
 });
 
-router.delete('/:id', (req, res)=>{
-  Article.findByIdAndRemove(req.params.id, (err, deletedArticle)=>{
-    // Now that we deleted the article from the Article Model's collection
-    // we now want to remove the article from the Author.articles
-    Author.findOne({'articles._id': req.params.id}, (err, foundAuthor) => {
-      // SO now that we found the Author with the article
-      // we are going to go into that authors array and find the article
-      // thats what the .id is doing (finding the article),
-      // remember req.params.id is the article.id
-      // then we are going to remove it
-      foundAuthor.articles.id(req.params.id).remove();
-      foundAuthor.save((err, data) => {
-         res.redirect('/articles');
-      });
-    })
-  });
+router.delete('/:id', async (req, res)=>{
+  try {
+
+        const deleteArticle = Article.findByIdAndRemove(req.params.id);
+        const findAuthor    = Author.findOne({'articles._id': req.params.id});
+
+        const [deletedArticle, foundAuthor] = await Promise.all([deleteArticle, findAuthor]);
+
+        foundAuthor.articles.id(req.params.id).remove();
+
+        await foundAuthor.save();
+
+        res.redirect('/articles');
+
+    } catch(err){
+
+      console.log(err)
+      res.send(err);
+    }
+
 });
 
-router.put('/:id', (req, res)=>{
-  // Check to see if the user, (req.body.authorId), changed the author
-  // if the did we'll need to update the newAuthro and remove the old author
+router.put('/:id', async (req, res)=>{
 
 
-  Article.findOneAndUpdate(req.params.id, req.body, {new: true}, (err, updatedArticle)=>{
-    // Once we updated the article in the articles collection
-    // then we want to update the article in Author.articles
-    Author.findOne({'articles._id': req.params.id}, (err, foundAuthor) => {
-      // so now that I've found the author that has the article
-      if(foundAuthor._id.toString() !== req.body.authorId){
-        // if they are not equal that means, the user changed the author
-        // so then we want to remove the article from the author that used
-        // to own it
-        foundAuthor.articles.id(req.params.id).remove()
-        foundAuthor.save((err, savedFoundAuthor) => {
-          // SO now that we removed the Article from the old authoer
-          // we want to find the New Author and add the update article to
-          // the new authors articles array
-          Author.findById(req.body.authorId, (err, newAuthor) => {
-            // so now that we've found the newAuthor,
-            // push the updatedArticle which if you'll look above was
-            // the response from the first database query
-            // Article.findOneAndUpdate
-            newAuthor.articles.push(updatedArticle);
-            newAuthor.save((err, savedNewAuthor) => {
-              res.redirect('/articles');
-            });
-          });
-        });
-      } else {
+  try {
 
-          // Find that article and remove
+    const findUpdatedArticle = Article.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    const findFoundAuthor = Author.findOne({'articles._id': req.params.id });
+
+    // For running pararrell async taks
+    const [updatedArticle, foundAuthor ] = await Promise.all([findUpdatedArticle, findFoundAuthor])
+
+    if(foundAuthor._id.toString() != req.body.authorId){
           foundAuthor.articles.id(req.params.id).remove();
-          // Then add the updateArticle to authors.articles array
-          foundAuthor.articles.push(updatedArticle);
-          foundAuthor.save((err, data) => {
-            res.redirect('/articles');
-          });
-      } // end of if else
-    });
-  });
-});
 
+          await foundAuthor.save();
+          const newAuthor = await Author.findById(req.body.authorId);
+          newAuthor.articles.push(updatedArticle);
+
+          const savedNewAuthor = await newAuthor.save();
+          // if you want to go back to the show page you can do something like this
+          res.redirect('/articles/' + req.params.id)
+
+
+    } else {
+
+          foundAuthor.articles.id(req.params.id).remove();
+          // push new one
+          foundAuthor.articles.push(updatedArticle);
+          // save the updated author to database
+          await foundAuthor.save()
+          res.redirect('/articles/' + req.params.id);
+
+    }
+
+
+  } catch (err){
+    res.send(err);
+  }
+
+});
 
 
 
